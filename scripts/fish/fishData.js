477f3fd940f18172c7ade99cd26c8ff99731bea3
Patrick Murphy <engine_ear@mac.com>
2020-06-03 12:00:07 -0500

    added holy,soldier and regular fish filters. Adjusted css to reflect same

--- scripts/fish/fishData.js
+++ scripts/fish/fishData.js
@@ -73,7 +73,7 @@ const fishCollection = [
     },
     {
         species: "Lion fish",
-        length: 13,
+        length: 15,
         name: "Spike",
         location: "Sea of Japan",
         food: "Other fish",
@@ -114,3 +114,28 @@ const fishCollection = [
     }
 
 ]
+const mostHolyFish = () => {
+    const holyFish = []
+    for (const fishOfThree of fishCollection) {
+        if (fishOfThree.length % 3 === 0 ) {
+            holyFish.push(fishOfThree)
+       }
+    } return holyFish
+}
+const soldierFish = () => {
+    const soldierfishArray = []
+    for (const soldierfishobject of fishCollection) {
+        if (soldierfishobject.length % 5 === 0 && soldierfishobject.length % 3 !== 0) {
+            soldierfishArray.push(soldierfishobject)
+        }
+    } return soldierfishArray
+}
+const nonholyfish = () => {
+    const regularFish = []
+    for (const unholy of fishCollection) {
+        if (unholy.length % 3 !== 0 && unholy.length % 5 !== 0) {
+           regularFish.push(unholy) 
+        }
+    } return regularFish
+}
+
